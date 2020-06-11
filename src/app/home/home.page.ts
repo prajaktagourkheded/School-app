6cc79f9dba43c2b8a584d32aa340e60d72ba9641
prajaktagourkheded <prajaktagourkhede@gmail.com>
2020-06-11 18:46:07 +0530

    4 screen with text update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChildren } from "@angular/core";
+import { IonSlides } from "@ionic/angular";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  @ViewChildren("mySlider") slider: IonSlides;
+  sliderOpts = {
+    slideShadows: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: true,
+    speed: 1000,
+    zoom: {
+      maxRatio: 5,
+    },
+  };
 
   constructor() {}
 
